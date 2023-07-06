03eb30f8c48915e0e49cb8520daadcecaaf5a1e5
Roshan Lal Dia <91119120+roshan-lal-dia@users.noreply.github.com>
2023-07-06 17:05:27 +0000

    added some style to render

--- app/page.tsx
+++ app/page.tsx
@@ -54,6 +54,27 @@ export default function Home() {
   };
 
 
-  return (<div>Hello there</div>)
-  
-  }
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between px-24 py-5">
+      <h1 className="text-5xl font-sans">Whisperer</h1>
+
+      <div className="flex  h-[35rem] w-[40rem] flex-col items-center bg-gray-600 rounded-xl">
+        <div className=" h-full flex flex-col gap-2 overflow-y-auto py-8 px-3 w-full">
+          <input type="file" accept=".wav, .mp3" onChange={handleFileChange} />
+
+          <div className="w-[90%] h-max border-2 break-words">
+            {isLoading ? "Loading..." : response ? response : ""}
+          </div>
+        </div>
+        <div className="relative  w-[80%] bottom-4 flex justify-center">
+          <button
+            onClick={callGetTranscription}
+            className="w-max bg-blue-500 px-4 py-2 rounded-sm "
+          >
+            Upload
+          </button>
+        </div>
+      </div>
+    </main>
+  );
+}
